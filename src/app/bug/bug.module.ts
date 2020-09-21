17d5c8da64140834d72d567546242b0c584f29e5
Kolaxis <ioannis.kolaxis@atos.net>
2020-09-21 16:55:19 +0300

    Created bug module & bugs-list component

---
+++ src/app/bug/bug.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BugsListComponent } from './bugs-list/bugs-list.component';
+
+
+
+@NgModule({
+  declarations: [BugsListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BugModule { }
---
+++ src/app/bug/bugs-list/bugs-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'atos-bugs-list',
+  templateUrl: './bugs-list.component.html',
+  styleUrls: ['./bugs-list.component.scss']
+})
+export class BugsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
