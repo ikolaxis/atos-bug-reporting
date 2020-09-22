9ace3d8f6d40efc4b3143df961161012c4209a8d
Kolaxis <ioannis.kolaxis@atos.net>
2020-09-22 15:43:10 +0300

    Bugs-list component remembers which table header was last selected for sorting, as well as the sort order

--- src/app/bug/bugs-list/bugs-list.component.ts
+++ src/app/bug/bugs-list/bugs-list.component.ts
@@ -9,17 +9,49 @@ import { BugModel } from '../bug.model';
 })
 export class BugsListComponent implements OnInit {
 
+  sortByHeader: string;
+  sortOrder: string;
+
   bugs: BugModel[];
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.sortByHeader = "title";
+    this.sortOrder = "desc";
+
     this.bugs = this.route.snapshot.data.bugs;
   }
 
   onClickSortByHeader(event?: MouseEvent): void {
-    const eventTarget = event ? (event.target as HTMLElement).textContent : '';
-    console.log("Clicked on " + eventTarget);
+    // Which table header did we click on?
+    const tableHeaderClicked = event ? (event.target as HTMLElement).textContent : '';
+
+    this.sortByHeader = tableHeaderClicked;
+
+    // If we clicked on the same table header as the one currently used for sorting,
+    // then we need to toggle the order, e.g. from "desc" to "asc" and vice versa.
+    this.sortOrder = (this.sortByHeader === tableHeaderClicked) ? this.toggleSortOrder(this.sortOrder) : 'desc';
+
+    console.log("Sort by: " + this.sortByHeader + ", Order: " + this.sortOrder);
+  }
+
+  toggleSortOrder(sortOrder: string): string {
+    let inverseSortOrder = 'desc';
+
+    switch (sortOrder) {
+      case 'desc':
+        inverseSortOrder = 'asc';
+        break;
+      case 'asc':
+        inverseSortOrder = 'desc';
+        break;
+      default:
+        console.log("Sort order is undefined. Using a descending order as default.");
+        break;
+    }
+
+    return inverseSortOrder;
   }
 
 }
