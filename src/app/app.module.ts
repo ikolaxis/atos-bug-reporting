6e5b78d5b72832309622e72ef0ac3cfd43b46714
Katerina Saranti <aikaterini.saranti@atos.net>
2020-09-23 18:32:17 +0300

    Refactoring - moving bug routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BugRoutingModule } from './bug-routing/bug-routing.module';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BugsListResolver } from './bug/bugs-list.resolver';
 import { BugModule } from './bug/bug.module';
+import { BugRoutingModule } from './bug/bug-routing.module';
 
 @NgModule({
   declarations: [
